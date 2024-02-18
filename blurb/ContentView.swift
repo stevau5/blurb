795f341103c24d9476b5a724d834f03a9ce055ef
stevau5 <stevenanthonyzanga@gmail.com>
2024-02-18 15:14:42 -0500

    Initial Commit

---
+++ blurb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blurb
+//
+//  Created by Steven Anthony Zanga on 2024-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blurb/blurbApp.swift
@@ -0,0 +1,17 @@
+//
+//  blurbApp.swift
+//  blurb
+//
+//  Created by Steven Anthony Zanga on 2024-02-18.
+//
+
+import SwiftUI
+
+@main
+struct blurbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

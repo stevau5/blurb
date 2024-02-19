4d9820668a0b4c8546481e7b75925fb31e5c2eff
stevau5 <stevenanthonyzanga@gmail.com>
2024-02-19 14:48:50 -0500

    Added profile view

---
+++ blurb/Components/SettingsRowView.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsRowView.swift
+//  blurb
+//
+//  Created by Steven Anthony Zanga on 2024-02-19.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    let imageName: String
+    let title: String
+    let tintColor: Color
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            Image(systemName: imageName)
+                .imageScale(.medium)
+                .foregroundColor(tintColor)
+            
+            Text(title)
+                .font(.subheadline)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+#Preview {
+    SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+}
---
+++ blurb/Core/Profile/ProfileView.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileView.swift
+//  blurb
+//
+//  Created by Steven Anthony Zanga on 2024-02-19.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        List {
+            Section {
+                HStack {
+                    Text("SZ")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 72, height: 72)
+                        .background(Color(.systemGray3))
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Steven Zanga")
+                            .fontWeight(.semibold)
+                            .padding(.top, 4)
+                        
+                        Text("stevenanthonyzanga@gmail.com")
+                            .font(.footnote)
+                            .accentColor(.gray)
+                    }
+                }
+            }
+            
+            Section("General") {
+                HStack {
+                    SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+                    Spacer()
+                    Text("1.0.0")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Section("Account") {
+                Button {
+                    print("sign out")
+                } label: {
+                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign out", tintColor: .red)
+                }
+                
+                Button {
+                    print("Delete account")
+                } label: {
+                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete accout", tintColor: .red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
